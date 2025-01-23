28fefa71393d2d64b8cf6858ca686e80c678adb0
Abdelrahman Omar <abdelrahman@e-scapes.me>
2025-01-23 17:16:20 +0200

    updates for product app

--- ins_gla/ins_apps/app_product/app_product.py
+++ ins_gla/ins_apps/app_product/app_product.py
@@ -6,9 +6,22 @@ class AppProduct(App):
         self.app: App = app
         super().__init__(app.ins)
 
+    def _image(ins , options , data):
+        uiadta = [
+            {"_data":data["th_main"],"class":"ins-col-12 ins-size-m", "_view": "image","style": "max-width:100px"}
+            
+        ]
+        return ins._ui._render(uiadta)
 
-    def _label(ins , options , data):
-        return data["title"]
+    def _title(ins,options,data):   
+        uiadta = [
+            {"_data":data["title"] ,"class":"ins-primary-color ins-col-12 ins-font-m"},
+            {"_data":data["weight"]+" GM","class":" ins-col-8 ins-text-center ins-title-xl ins-strong ins-font-s ins-tag ins-primary-color ins-flex-center"},
+            {"_data":data["kart"]+" K","class":"ins-col-8 ins-text-center ins-title-xl ins-strong ins-font-s  ins-tag ins-primary-color ins-flex-cenetr"},
+        ]
+        return ins._ui._render(uiadta)
+    
+
 
     def out(self):
         
