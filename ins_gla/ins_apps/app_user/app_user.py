2593bdc330a71550406d0068b73ffa432228cfb3
Abdelrahman Omar <abdelrahman@e-scapes.me>
2025-03-04 21:46:09 +0200

    apps for prices and users

---
+++ ins_gla/ins_apps/app_user/app_user.py
@@ -0,0 +1,42 @@
+from ins_kit._engine._bp import App
+
+
+class AppUser(App):
+    def __init__(self, app) -> None:
+        self.app: App = app
+        super().__init__(app.ins)
+
+    def out(self):
+
+        ops = self.ins._apps._crud_ops
+
+       
+       
+        def func(data: dict):
+            if "password" in data:
+                if data["password"] == "":
+                    del data["password"]
+                else :
+                    data["password"] = d = self.ins._data.hash_password(
+                        data["password"])
+            return data
+
+        ops._form_befor_insert = func
+        ops._form_befor_update = func
+        
+        
+        def ufunc(data: dict):
+            if "password" in data:
+                    del data["password"]
+            return data
+           
+
+        
+        ops._form_befor_update_data = ufunc
+        
+
+        data = self.ins._db._get_row("kit_user", "*", "groups = 4")
+
+
+        r = self.ins._apps._crud(ops, properties=self.app._properties)
+        return r
