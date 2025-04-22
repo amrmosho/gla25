a0602ccec1cf7831778d13b8cb6cca0a02d75eca
amrmosho <empcland@gmail.com>
2025-04-22 13:09:57 +0200

    update

---
+++ ins_cg/ins_apps/app_checkout/app_payment.py
@@ -0,0 +1,197 @@
+from ins_gla.ins_kit._elui import ELUI
+from ins_kit._engine._bp import App
+p = "/ins_web/ins_uploads/"
+
+class AppPayment(App):
+    def __init__(self, app) -> None:
+        self.app: App = app
+        super().__init__(app.ins)
+    @property
+    def session_payment_name(sel):
+        return "glapayment"
+    
+    @property
+    def session_name(sel):
+        return "glaproducts"
+    
+    @property
+    def session_address_name(sel):
+        return "glaaddress"
+
+
+    def items_area(self,string= False):
+        ## Items Area
+        uidata = []
+        sedata=self.ins._server._get_session(self.session_name)
+        shipping = self.ins._db._get_row("gla_settings","shipping_fees","id=1")["shipping_fees"]
+
+        rq = self.ins._server._req()
+        subtotal = 0
+        chargs = 0
+        for k,v in sedata.items():
+            subtotal+= float(v["price"]) * float(v["count"])
+            uidata+= ELUI(self.ins).small_pro_block(v)
+            uidata.append({"class":"ins-space-m"})
+        uidata.append({"class":"ins-space-m"})
+        uidata.append({"class":"ins-line ins-col-12 not-for-phone"})
+        uidata.append({"class":"ins-space-m"})
+        uidata.append({"_data": "Voucher", "_data-ar":" قسيمة","_trans":"true","class": "ins-col-12  ins-grey-d-color ins-strong-m  not-for-phone "})
+        uidata.append({"_type": "input","type":"text","placeholder":"code","placeholder-ar":" رمز","_trans":"true","name":"voucher","pclass":"ins-col-12  not-for-phone","style":"    background: white;border-radius:4px;"})
+        uidata.append({"class":"ins-space-xl"})
+        uidata.append({"start": "true", "class": "ins-flex ins-col-12 -fees-info ins-padding-m","style":"border-radius:8px !important;border: 1px solid var(--grey-l);"})
+        uidata.append({"_data": "Your details","_data-ar":"تفاصيلك","_trans":"true", "class": "ins-col-12 ins-title-s ins-grey-d-color ins-strong-l "})
+        uidata.append({"class":"ins-space-s"})
+        uidata.append({"_data": "Subtotal", "_data-ar":"المجموع الفرعي","_trans":"true","class": "ins-col-6 ins-m-col-6  ins-title-xs  ins-grey-color ins-strong-m"})
+        uidata.append({"_data": str(subtotal),"data-value" : subtotal,"_view":"currency","_currency_symbol":" EGP","_currency_symbol_ar":" جنيه", "class": "ins-col-6  ins-m-col-6 ins-grey-d-color ins-title-xs ins-strong-l ins-flex-end -subtotal-text"})
+        pclass = "ins-hidden"
+
+        total = subtotal  
+        uidata.append({"start": "true", "class": f"-online-payment-fee  ins-col-12 ins-flex {pclass}"})
+        uidata.append({"_data": "Online payment fee","_data-ar": "رسوم الدفع عبر الإنترنت", "_trans":"true","class": "ins-col-6  ins-m-col-6 ins-title-xs  ins-grey-color ins-strong-m"})
+        uidata.append({"_data": str(chargs),"data-value" : chargs,"_view":"currency","_currency_symbol":" EGP","_currency_symbol_ar":" جنيه", "class": "ins-col-6  ins-m-col-6 ins-grey-d-color ins-title-xs ins-strong-l ins-flex-end -chargs-text"})
+        uidata.append({"end": "true"})
+        uidata.append({"_data": "Shipping", "_data-ar":" شحن","_trans":"true", "class": "ins-col-6  ins-m-col-6 ins-title-xs  ins-grey-color ins-strong-m"})
+        if total > 200000:
+          uidata.append({"_data": "Free","_data-ar": "مجاني","_trans":"true","data-value" : 0, "class": "ins-col-6  ins-m-col-6 ins-gold-d-color ins-title-xs ins-strong-l ins-flex-end  -shipping-text"})
+        else:
+          uidata.append({"_data": str(shipping),"data-value" : int(shipping),"_view":"currency","_currency_symbol":" EGP","_currency_symbol_ar":" جنيه",  "class": "ins-col-6  ins-m-col-6 ins-gold-d-color ins-title-xs ins-strong-l ins-flex-end -shipping-text"})
+          total +=int(shipping)
+        uidata.append({ "class": "ins-line ins-col-12"})
+        uidata.append({"_data": "Total", "_data-ar":" المجموع","_trans":"true", "class": "ins-col-6  ins-m-col-6 ins-title-xs  ins-grey-color ins-strong-m"})
+        uidata.append({"_data":  str(total),"_view":"currency","_currency_symbol":" EGP","_currency_symbol_ar":" جنيه", "class": "ins-col-6  ins-m-col-6 ins-grey-d-color ins-title-xs ins-strong-l ins-flex-end -total-text"})
+        uidata.append({"end": "true"})
+        uidata.append({"class":"ins-space-xl"})
+
+        back_url = self.ins._server._url({"mode":"delivery"},["id"])
+        uidata.append({"start": "true", "class": "ins-flex ins-col-12  ins-padding-m  -address-card","style":"border-radius:8px !important;border: 1px solid var(--grey-l);"})
+        asession = self.ins._server._get_session(self.session_address_name)
+        if type(asession) == dict and "type" in asession and  asession["type"] == "store":
+            
+            stores = self.ins._db._get_data("gla_address","*",update_lang=True)
+            for s in stores:
+               if str(s["id"]) == str(asession["id"]):
+                  store = s
+            ainfo = [
+            {"_data": "Pickup Address","_data-ar": "عنوان الاستلام ", "_trans":"true","class": "ins-col-12 ins-title-s ins-grey-d-color ins-strong-l "},
+            {"class":"ins-space-s"},
+            {"_data": store.get("title",""),"class": "ins-col-12  ins-title-20	  ins-grey-d-color ins-strong-l"},
+            {"_data": store.get("address",""), "class": "ins-col-12 ins-grey-color"},
+            {"_data": f"Phone: {store['phone']} | WhatsApp: {store['whatsapp']} | Email:  {store['email']}" ,"_data-ar": f"هاتف: {store['phone']} | واتساب: {store['whatsapp']} | بريد الكتروني:  {store['email']}" ,"_trans":"true","class":"ins-col-12 ins-grey-d-color ins-strong-m ins-title-14 not-for-phone"},
+            {"end": "true"},
+            {"class":"ins-space-xl"}
+            ]
+        else:
+           address = self.ins._db._get_row("gla_user_address","*",f"id='{asession['id']}'")
+           ainfo = [
+           {"_data": "Shipping Address","_data-ar":"عنوان الشحن","_trans":"true", "class": "ins-col-6 ins-title-s ins-grey-d-color ins-strong-l ins-m-col-6"},
+           {"class":"ins-space-s"},
+           {"_data": address.get("title",""), "class": "ins-col-12  ins-title-20	  ins-grey-d-color ins-strong-l"},
+           {"_data": address.get("address",""), "class": "ins-col-12 ins-grey-color"},
+           {"_data": f"Mobile: {address.get('phone','')} | Email: {address.get('email','')}", "class": "ins-col-12 ins-grey-d-color ins-strong-m ins-title-14 not-for-phone"},
+           {"_data": f"Mobile: {address.get('phone','')} ", "class": "ins-col-12 ins-grey-d-color ins-strong-m ins-title-14 not-for-web"},
+           {"end": "true"},
+           {"class":"ins-space-xl"}
+           ]
+        uidata+=ainfo
+        if subtotal < 20000:
+             uidata.append({"_data": "Place Order <img src='"+p+"style/right_arrow.svg'></img>","_data-ar":"اتمام الشراء","_trans":"true","class": "ins-button-s ins-flex-center ins-title-xs ins-strong-m ins-flex-grow ins-grey-l  ins-text-upper","style":"    height: 46px;color:white;"})
+             remaining = 20000 - subtotal
+             n = self.ins._data._format_currency(float(remaining), symbol=False)
+             remaining = f"{n} EGP"
+      
+             msg = f"Add {remaining} to place your order"
+             if self.ins._langs._this_get()["name"] == "ar":
+                 remaining = f"{n} جنيه"
+                 msg = f"أضف {remaining} لإتمام طلبك"
+             uidata.append({"_data": msg,"class":"ins-col-12  ins-text-center"})
+        else:
+           uidata.append({"_data": "Place Order <img src='"+p+"style/right_arrow.svg'></img>","_data-ar": "اتمام الشراء ","_trans":"true","class": "ins-button-s ins-flex-center ins-title-xs ins-strong-m ins-flex-grow ins-gold-d  ins-text-upper -submit-order-btn","style":"    height: 46px;    border: 1px solid var(--primary-d);"})
+       
+
+        
+        
+        
+        
+        
+        
+        
+        uidata.append({"href":back_url,"_type":"a","_data": " <img src='"+p+"style/left_arrow.svg'></img> Back", "_data-ar":"رجوع","_trans":"true","class": "ins-button-s ins-flex-center ins-strong-m ins-text-upper ins-gold-d-color   ins-col-12 ins-title-xs	-back-btn","style":"    height: 46px;"})
+        uidata.append({"_data": "Your info will be saved to a Shop account. By continuing, you agree to Shop’s <a>Terms of Service</a> and acknowledge the  <a>Privacy Policy</a>.", "_data-ar":"سيتم حفظ معلوماتك في حساب المتجر. من خلال الاستمرار، فإنك توافق على سياسة المتجر","_trans":"true","class": " ins-col-12 ins-grey-color ","style":"line-height:24px"})
+        
+        
+        if string:
+         return self.ins._ui._render( uidata)
+        else:
+         return uidata
+
+
+    def ui(self):
+        pdata = self.ins._server._get_session(self.session_payment_name)
+        sclass = "ins-hidden"
+        if pdata and pdata["type"] !="":
+          ddata = self.ins._db._get_row("gla_payment_methods","*",f"id='{pdata['type']}'")
+          if ddata["charges"] !="":
+            sclass = ""
+
+       
+        uidata=[{"start":"true","class":"ins-col-7 ins-flex ins-padding-2xl"}]
+        uidata.append({"start":"true","class":"ins-col-12 ins-flex"})
+        uidata.append({"start":"true","class":"ins-col-12 ins-gap-o"})
+        uidata.append({"_data":"payment","_data-ar":"دفع","_trans":"true","class":"ins-col-12 ins-title-m		 ins-strong-m ins-grey-d-color ins-text-upper"})
+        uidata.append({"_data":"All transactions are secure and encrypted","_data-ar":"جميع المعاملات آمنة ومشفرة","_trans":"true","class":"ins-col-12 ins-title-xs ins-grey-color -step-des"})
+        
+        
+        uidata.append({"_data":" Checkout charges may apply","_data-ar":"قد يتم تطبيق رسوم الدفع الالكتروني","_trans":"true","class":f"ins-col-12 ins-card ins-info -extra-fees-card {sclass}"})
+        
+        uidata.append({"end":"true"})
+        uidata.append({"class":"ins-space-s"})
+        
+        
+        
+        payments = self.ins._db._get_data("gla_payment_methods","*",update_lang=True)
+
+        i = 0
+        for payment in payments:
+            i +=1
+            img = "style/radio.svg"
+            pclass = ""
+            if pdata:
+                if str(pdata["type"]) == str(payment["id"]):
+                   img = "style/radio_checked_b.svg"
+                   pclass = "ins-active"
+
+
+            pcard = [
+                     {"start":"true","class":"ins-col-12 ins-flex ins-gap-o"},
+                     {"start":"true","data-name":payment["id"],"class":f"{pclass} ins-col-12 ins-flex-center -payment-type-btn ins-m-flex-start {payment.get('class','')}"},
+                     {"_type":"img","src":f"{p}{img}","loading":"lazy","class":"-payment-type-btn-img"},
+                     {"_data": payment["title"],"class":"ins-strong-m ins-grey-m-color payment-title"},
+               ]
+            if payment["charges_type"] !="" and payment["charges"] !="":
+               pcard = [
+                     {"start":"true","class":"ins-col-12 ins-flex ins-gap-o"},
+                     {"start":"true","data-charges_type":payment["charges_type"],"data-charges":payment["charges"],"data-name":payment["id"],"class":f"{pclass} ins-col-12 ins-flex-center -payment-type-btn ins-m-flex-start {payment.get('class','')}"},
+                     {"_type":"img","src":f"{p}{img}","loading":"lazy","class":"-payment-type-btn-img"},
+                     {"_data": payment["title"],"class":"ins-strong-m ins-grey-m-color payment-title"},
+               ]
+           
+            pcard.append({"class":"ins-col-grow ins-m-col-6"})
+            if payment['logo']:
+               pcard.append({"_type":"img","src":f"{p}{payment['logo']}","loading":"lazy","class":"ins-flex-end -payment-img","style":"max-width:40px"})
+            
+            pcard.append({"end":"true"})
+            pcard.append({"_data":payment["des"],"class":f"ins-col-12 ins-title-xs ins-grey-color ins-text-none -payment-subtype-area -payment-subtype-area-{payment['id']} {pclass}"})
+            pcard.append({"end":"true"})
+            uidata+=pcard
+        uidata.append({"end":"true"})
+        uidata.append({"end":"true"})
+        uidata.append({"start":"true","class":"ins-col-5 ins-gap-o ins-flex -items-area  ins-padding-2xl","style":"background:white;   border-left: 1px solid var(--primary-l);"})
+        uidata+=self.items_area()
+        uidata.append({"end":"true"})
+        return uidata
+   
+   
+
+    def out(self):
+
+        return self.ui()
---
+++ ins_cg/ins_apps/app_users/app_users_profile.py
@@ -0,0 +1,127 @@
+from ins_gla.ins_kit._gusers import Gusers
+from ins_kit._engine._bp import App
+from ins_gla.ins_kit._elui import ELUI
+
+class AppUsersProfile(App):
+    def __init__(self, app) -> None:
+        self.app: App = app
+        self.user = Gusers(app.ins)
+        super().__init__(app.ins)
+
+
+
+
+
+
+    def user_setting(self, user):
+        
+        return [
+            {"start": "true", "class": "ins-col-12 ins-flex-end ins-padding-2xl "},
+            {"_type": "input", "required": "true", "value": user["first_name"], "title": "First Name","title-ar":"الاسم الأول","_trans":"true", "placeholder": "Enter First Name", "placeholder-ar": "أدخل الاسم الأول", "type": "text", "name": "first_name", "class": "-signup-first-name-inpt", "pclass": "ins-col-12"},
+            {"_type": "input", "required": "true", "value": user["last_name"], "title": "Last Name","title-ar":"اسم العائلة","_trans":"true", "placeholder": "Enter Last Name", "placeholder-ar": "أدخل اسم العائلة", "type": "text", "name": "last_name", "class": "-signup-last-name-inpt", "pclass": "ins-col-12"},
+            {"class": "ins-line ins-col-12"},
+            {"_data": "Update","_data-ar":" تحديث","_trans":"true", "class": "ins-button-m ins-gold-d ins-col-2 ins-flex-center -update-name-btn"},
+            {"end": "true"}
+
+        ]
+
+    def change_password(self, data):
+        return [
+            {"start": "true", "class": "ins-col-12 ins-flex-end ins-padding-2xl "},
+            {"_type": "input", "required": "true", "title": "Old Password","title-ar":" كلمة المرور القديمة","_trans":"true", "_end": '<i class="-show-old-password lni lni-eye"></i>', "placeholder": "Enter Old Password", "placeholder-ar": " كلمة المرور القديمة", "type": "password", "name": "old_password", "class": "-update-old-password-inpt", "pclass": "ins-col-12"},
+            {"_type": "input", "required": "true", "title": "Password","title-ar":" كلمة المرور","_trans":"true", "_end": '<i class="-show-password lni lni-eye"></i>', "placeholder": "Enter Password", "type": "password", "placeholder-ar": " كلمة المرور ",  "name": "password", "class": "-update-password-inpt", "pclass": "ins-col-12"},
+            {"_type": "input", "required": "true", "title": "Confirm Password","title-ar":"تأكيد كلمة المرور","_trans":"true", "_end": '<i class="-show-confirm-password lni lni-eye"></i>', "placeholder": "Confirm Password",  "placeholder-ar": "تأكيد كلمة المرور","type": "password", "name": "confirm_password", "class": "-update-confirm-password-inpt", "pclass": "ins-col-12"},
+            {"class": "ins-line ins-col-12"},
+            {"_data": "Update","_data-ar":" تحديث","_trans":"true", "class": "ins-button-m ins-gold-d ins-col-2 ins-flex-center -update-password-btn"},
+            {"end": "true"}
+
+        ]
+
+    def change_email(self, data):
+        u = self.user._check()
+        udata = self.ins._db._get_row("kit_user","email,email_status",f"id='{u['id']}'")
+        uidata = [            {"start": "true", "class": "ins-col-12 ins-flex ins-padding-2xl "},
+            {"_type": "input", "value": data.get("email", ""), "title": "Email","title-ar":"بريد إلكتروني","_trans":"true", "placeholder": "Enter Email","placeholder-ar": "ادخل البريد الالكتروني", "type": "email", "name": "email", "class": "-update-email-inpt", "pclass": "ins-col-8 ins-m-col-6"},
+            {"start": "true", "class": " -verified-area ins-col-4  ins-m-col-6"},
+            ]
+        
+        ui_msg = "Verified Email <i class='lni lni-check ins-font-l'></i>"
+        if self.ins._langs._this_get()["name"] == "ar":
+                    ui_msg = "تم التحقق  <i class='lni lni-check ins-font-l'></i> "
+
+
+
+        if udata["email_status"] != "verified":
+         uidata.append({"_data": "Send Verification Code","_data-ar":"ارسال رمز التحقق","_trans":"true", "class": "ins-button-m ins-strong-m   ins-gold-bg  ins-col-12 -send-email-veri-btn ins-flex-center", "style": " margin-top: 35px;"})
+        else:
+         uidata.append({"_data": ui_msg,"_trans":"true", "class": " ins-strong-m   ins-col-12 ins-flex-center ins-border ins-radius-m", "style": " margin-top: 35px;min-height:40px"})
+
+       
+        uidata+= [   
+                        {"end": "true"},
+
+
+             {"class": "ins-line ins-col-12"},
+            
+            {"end": "true"}]
+        
+        return uidata
+
+       
+
+    def out(self, ins):
+        g = self.ins._server._get()
+
+        menus = [
+            {"name": "user_setting", "title": "User Settings","title-ar":"إعدادات المستخدم", "icon": "lni-user-4"},
+            {"name": "change_password", "title": "Change Password","title-ar":" تغيير كلمة المرور", "icon": "lni-locked-1"},
+            {"name": "change_email", "title": "Change Email","title-ar":" تغيير البريد الإلكتروني","icon": "lni-envelope-1"}
+        ]
+
+        usmenu = [{"start": "true", "class": "ins-col-12 ins-flex ins-padding-xl"}]
+
+        for m in menus:
+            bclass = "ins-gold-d-color" if g.get("id") == m["name"] else ""
+            aclass = "ins-strong-l" if g.get("id") == m["name"] else ""
+            url = self.ins._server._url({'alias':'puser','mode':'profile','id':m['name']})
+            text = f"<i class='lni ins-font-l {bclass} {m['icon']}'></i>  {m['title']}"
+            if self.ins._langs._this_get()["name"] == "ar":
+                text =f"<i class='lni ins-font-l {bclass} {m['icon']}'></i>  {m['title-ar']}"
+            usmenu.extend([
+                {"start": "true", "class": f" ins-col-12 ins-primary-w ins-flex ins-border ins-radius-l ins-padding-m"},
+                {"_type": "a", "href":url , "_data":text , "class": f"{bclass} {aclass} ins-title-xs ins-col-12"},
+                {"end": "true"}
+            ])
+
+        usmenu.append({"end": "true"})
+
+        border = "ins-border-right"
+        if self.ins._langs._this_get()["name"] == "ar":
+                    border = "ins-border-left"
+
+        uidata = [
+            {"class": "ins-space-l"},
+            {"start": "true", "class": "gla-container ins-flex-center ins-padding-2xl  ins-card ins-flex-valign-start"},
+            {"start": "true", "class": f"ins-col-4 ins-flex ins-text-upper {border}"},
+        ]
+
+        uidata += usmenu
+        uidata += [
+            {"end": "true"},
+            {"start": "true", "class": "ins-col-8 ins-flex"},
+        ]
+
+        u = self.user._check()
+        user = self.ins._db._get_row("kit_user","*",f"id='{u['id']}'")
+
+
+        if g.get("id") == "user_setting":
+            uidata += self.user_setting(user)
+        elif g.get("id") == "change_password":
+            uidata += self.change_password(user)
+        elif g.get("id") == "change_email":
+            uidata += self.change_email(user)
+
+        uidata.append({"end": "true"})
+
+        return self.ins._ui._render(uidata)
